d404b1a27a492d1b6ecf66840d554fdba6276932
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-15 18:15:02 -0300

    idade

---
+++ atv11.py
@@ -0,0 +1,15 @@
+# Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+# Criança (0-12 anos)
+# Adolescente (13-17 anos)
+# Adulto (18-59 anos)
+# Idoso (60 anos ou mais)
+
+idade = int(input("Digite sua idade:"))
+if idade<=12:
+    print("Criança")
+elif idade<=17:
+    print("Adolecente")
+elif idade<=59:
+    print("Adulto")
+else:
+    print("Idoso")
